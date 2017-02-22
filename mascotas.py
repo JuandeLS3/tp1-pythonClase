1feb2dfd1dc84ff2ef927fdf526f8251d2c8cf9b
Juan <mrjuancod94@gmail.com>
2017-02-22 09:43:54 +0100

    Añadido neuvos cript con clases

---
+++ mascotas.py
@@ -0,0 +1,8 @@
+class mascota:
+   numero_de_patas = 0
+   color = "marron"
+
+perro = mascota()
+perro.numero_de_patas = 4,
+perro.color = "negro",
+print "El perro tiene " + perro.numero_de_patas + " patas y es de color " + perro.color
