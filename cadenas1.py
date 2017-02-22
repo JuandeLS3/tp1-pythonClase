60f546897202b5418a4f5fef315f8fb7911e1172
Juan <mrjuancod94@gmail.com>
2017-02-22 09:45:30 +0100

    Delete cadenas1.py

--- cadenas1.py
+++
@@ -1,9 +0,0 @@
-class CadenasTp1:
-    algo=[]
-    cad=raw_input('Escribe un nombre: ')
-    print cad, ' tiene ',len(cad), ' caracteres '
-    print 'La cadena ',cad,' escrita al reves es ', cad[::-1]
-    # Creando array de Strings
-    algo=["clases","python","pelo","aula",cad,"cielo"]
-    for var in algo:
-        print var
